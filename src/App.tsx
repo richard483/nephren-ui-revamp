3091b44f61c88edb343307267f5c3589d043c2f1
richard483 <richard.william483@gmail.com>
2024-11-11 01:27:46 +0700

    intial commit

---
+++ src/App.tsx
@@ -0,0 +1,63 @@
+import './App.scss';
+
+function App() {
+  return (
+    <>
+      <span id="box-grey" />
+      <span id="box-blue" />
+      <div className="dots">
+        <span className="dot" />
+        <span className="dot-text">e</span>
+        <span className="dot" />
+        <span className="dot" />
+        <span className="dot" />
+        <span className="dot" />
+        <span className="dot" />
+        <span className="dot-text">n</span>
+        <span className="dot" />
+        <span className="dot" />
+        <span className="dot" />
+        <span className="dot" />
+        <span className="dot" />
+        <span className="dot" />
+        <span className="dot" />
+        <span className="dot" />
+        <span className="dot-text">p</span>
+        <span className="dot" />
+        <span className="dot" />
+        <span className="dot" />
+        <span className="dot" />
+        <span className="dot" />
+        <span className="dot" />
+        <span className="dot" />
+        <span className="dot" />
+        <span className="dot" />
+        <span className="dot-text">e</span>
+        <span className="dot" />
+        <span className="dot" />
+        <span className="dot" />
+        <span className="dot" />
+        <span className="dot" />
+        <span className="dot-text">r</span>
+        <span className="dot" />
+        <span className="dot" />
+        <span className="dot" />
+        <span className="dot" />
+        <span className="dot" />
+        <span className="dot-text">h</span>
+        <span className="dot" />
+        <span className="dot" />
+        <span className="dot" />
+        <span className="dot" />
+        <span className="dot" />
+        <span className="dot" />
+        <span className="dot" />
+        <span className="dot" />
+        <span className="dot" />
+        <span className="dot-text">n</span>
+      </div>
+    </>
+  );
+}
+
+export default App;
