3076179f1376101fe3b97f6ba44815940b7c483f
richard483 <richard.william483@gmail.com>
2024-12-07 15:10:41 +0700

    feat: added google analytics

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,7 +1,15 @@
 import HeroSection from '@component/HeroSection/HeroSection.tsx';
 import BorderLine from '@component/BorderLine/BorderLine.tsx';
+import ReactGA from 'react-ga4';
+import { useEffect } from 'react';
+
+const configValue: string = import.meta.env.VITE_GOOGLE_TAG as string;
 
 function App() {
+  useEffect(() => {
+    ReactGA.initialize(configValue);
+    ReactGA.send('pageview');
+  });
   return (
     <>
       <BorderLine />
