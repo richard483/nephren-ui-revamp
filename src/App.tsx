a55c86a8680c19f724628fd6afd607c575ae9950
richard483 <richard.william483@gmail.com>
2025-02-10 10:30:21 +0700

    fix: moved google analytic to /app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import LandingPage from './pages/LandingPage';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { lazy } from 'react';
+import { lazy, useEffect } from 'react';
 import BorderLine from './component/BorderLine/BorderLine';
+import ReactGA from 'react-ga4';
 
 const RemoteBlogs = lazy(() => import('blogs/App'));
+const configValue: string = import.meta.env.VITE_GOOGLE_TAG as string;
 
 function App() {
+  useEffect(() => {
+    ReactGA.initialize(configValue);
+    ReactGA.send('pageview');
+  });
   return (
     <BrowserRouter>
       <BorderLine />
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,14 +1,6 @@
 import HeroSection from '@component/HeroSection/HeroSection.tsx';
-import ReactGA from 'react-ga4';
-import { useEffect } from 'react';
-
-const configValue: string = import.meta.env.VITE_GOOGLE_TAG as string;
 
 function LandingPage() {
-  useEffect(() => {
-    ReactGA.initialize(configValue);
-    ReactGA.send('pageview');
-  });
   return (
     <>
       <HeroSection />
