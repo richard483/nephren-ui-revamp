7a473412cdd8ef854b41ae39179d61c028269403
richard483 <richard.william483@gmail.com>
2025-04-30 15:52:25 +0700

    Revert "test: removed react ga for test purpose"
    This reverts commit c0439c3b1fde6d50309fdaf227c14ac2892a08a4.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import LandingPage from './pages/LandingPage';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { lazy } from 'react';
+import { lazy, useEffect } from 'react';
 import BorderLine from './component/BorderLine/BorderLine';
+import ReactGA from 'react-ga4';
 import BorderLineRight from './component/BorderLineRight/BorderLineRight';
 
 const RemoteBlogs = lazy(() => import('blogs/App'));
+const configValue: string = import.meta.env.VITE_GOOGLE_TAG as string;
 
 function App() {
+  useEffect(() => {
+    ReactGA.initialize(configValue);
+    ReactGA.send('pageview');
+  });
   return (
     <div id="app">
       <BrowserRouter>
