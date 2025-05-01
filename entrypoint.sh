ee8e9b6560f04f169d82f40d68608b8bf4a3a53a
richard483 <richard.william483@gmail.com>
2025-05-01 21:51:52 +0700

    kube: tried to implement more dynamic env variable

---
+++ entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+ # Replace placeholders with environment variables
+ sed -i "s|__VITE_GOOGLE_TAG__|${VITE_GOOGLE_TAG}|g" /app/dist/js/*.js
+ sed -i "s|__VITE_NEPHREN_BLOG__|${VITE_NEPHREN_BLOG}|g" /app/dist/js/*.js
+ 
+ # Start the server
+ serve -s dist -l 7002
