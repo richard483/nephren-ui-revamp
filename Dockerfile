7b7aa968a5ef97f16ae50a4ca42b086df9dbb5b2
richard483 <richard.william483@gmail.com>
2025-05-02 14:06:51 +0700

    patch: updated nginx conf, dockerfile, env sh

--- Dockerfile
+++ Dockerfile
@@ -8,9 +8,9 @@ RUN npm run build
 
 FROM nginx:1.21.6-alpine
 COPY --from=build /app/dist /usr/share/nginx/html
-COPY /nginx.conf /etc/nginx/conf.d/default.conf
+COPY nginx.conf /etc/nginx/conf.d/default.conf
 COPY env.sh /docker-entrypoint.d/env.sh
-RUN chmod +x /docker-entrypoint.d/env.sh
-
+RUN chmod +x /docker-entrypoint.d/env.sh && \
+    chmod -R a+rwx /docker-entrypoint.d/  # Ensure proper permissions
 ENTRYPOINT ["/docker-entrypoint.sh"]
 CMD ["nginx", "-g", "daemon off;"]
--- env.sh
+++ env.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
-for i in $(env | grep ENV_VAR_) // Make sure to use the prefix ENV_VAR_ if you have any other prefix in env.production file varialbe name replace it with ENV_VAR_
-do
-    key=$(echo $i | cut -d '=' -f 1)
-    value=$(echo $i | cut -d '=' -f 2-)
-    echo $key=$value
-    # sed All files
-    # find /usr/share/nginx/html -type f -exec sed -i "s|${key}|${value}|g" '{}' +
+set -e  # Exit on error
 
-    # sed JS and CSS only
-    find /usr/share/nginx/html -type f \( -name '*.js' -o -name '*.css' \) -exec sed -i "s|${key}|${value}|g" '{}' +
+echo "Starting environment variable substitution"
+
+# Process all environment variables with ENV_VAR_ prefix
+for i in $(printenv | grep '^ENV_VAR_'); do
+    key=$(echo "$i" | cut -d '=' -f 1)
+    value=$(echo "$i" | cut -d '=' -f 2-)
+    
+    echo "Replacing $key with $value"
+    
+    # Process all files including HTML
+    find /usr/share/nginx/html -type f \( -name '*.js' -o -name '*.css' -o -name '*.html' \) \
+        -exec sed -i "s|$key|$value|g" {} +
 done
+
+echo "Environment variable substitution completed"
